a33d36272315700c2e1ca1a597bb263c0883b6e2
henritresor <shimwamanahenritresor@gmail.com>
2023-04-26 23:41:43 +0530

    replace browserrouter with hashrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import AuthContext from './context/AuthContext.jsx'
 import AppContext from './context/AppContext.jsx'
 import '@fontsource/roboto/500.css';
-import { BrowserRouter as Router} from 'react-router-dom'
+import { HashRouter as Router} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
