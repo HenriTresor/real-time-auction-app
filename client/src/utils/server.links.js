bc5c536b913bdf19bb6e0c704c5390b6a7a4f568
henritresor <shimwamanahenritresor@gmail.com>
2023-04-26 23:03:58 +0530

    changed user to User

--- client/src/utils/server.links.js
+++ client/src/utils/server.links.js
@@ -1,4 +1,5 @@
-export const rootLink = 'http://localhost:7000'
+// export const rootLink = 'http://localhost:7000'
+export const rootLink = 'https://auct-server.onrender.com'
 export const loginLink = `${rootLink}/auth/login`
 export const usersLink = `${rootLink}/users`
 export const auctionsLink = `${rootLink}/auctions`
